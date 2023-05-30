efc29980a270780b7f50009c0884d97e2d824483
Adi <96goldner@gmail.com>
2023-05-30 23:13:24 +0300

    critical  fix angular modle imports

--- front/form-gen/src/app/app.module.ts
+++ front/form-gen/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { ReactiveFormsModule } from '@angular/forms'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SimpleFormComponent } from './simple-form/simple-form.component';
-import { EditableTableFormComponent } from './editable-table-form/editable-table-form.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { FooterComponent } from './footer/footer.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -16,7 +15,6 @@ import { MatSelectModule } from '@angular/material/select';
   declarations: [
     AppComponent,
     SimpleFormComponent,
-    EditableTableFormComponent,
     NavBarComponent,
     FooterComponent
   ],
